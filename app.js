7983854080bc17eae1f6cf255851b0623c9b2175
Ali Jahankah <aral2@yahoo.com>
2022-09-24 21:36:26 +0100

    using morgan to log

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const dotEnv = require("dotenv");
+const morgan = require("morgan");
 
 const indexRoute = require("./routes/index");
 const app = express();
@@ -16,6 +17,10 @@ app.set("views", "views");
 // Set Statics
 app.use(express.static(path.join(__dirname, "public")));
 
+//Log with Morgan in Dev Mode
+if (process.env.NODE_ENV === "developement") {
+  app.use(morgan("dev"));
+}
 // Routes
 app.use(indexRoute);
 const PORT = process.env.PORT || 5000;
