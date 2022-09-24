70bd62bdd84d89a1a4b533a7eb08629a13561ca8
Ali Jahankah <aral2@yahoo.com>
2022-09-24 18:33:51 +0100

    adding cross env and dotenv to set envs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require("express");
-
 const path = require("path");
+
+const express = require("express");
+const dotEnv = require("dotenv");
+
 const indexRoute = require("./routes/index");
 const app = express();
-const PORT = 5000;
+
+// Load Config
+dotEnv.config({ path: "./config/config.env" });
 
 // View Engine
 app.set("view engine", "ejs");
@@ -14,6 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use(indexRoute);
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`App is running on ${PORT}`);
+  console.log(`App is running on ${PORT} in ${process.env.NODE_ENV} Mode.`);
 });
