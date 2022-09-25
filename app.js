c84b679ad41cbf7fbea0cb7c4effdc183ae66ed0
Ali Jahankah <aral2@yahoo.com>
2022-09-25 21:46:54 +0100

    using fontawsome and bootstrap and mongo DB

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const connectDB = require("./config/db");
 
 const express = require("express");
 const dotEnv = require("dotenv");
@@ -9,14 +10,15 @@ const app = express();
 
 // Load Config
 dotEnv.config({ path: "./config/config.env" });
-
+connectDB();
 // View Engine
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 // Set Statics
 app.use(express.static(path.join(__dirname, "public")));
-
+// app.use(express.static(path.join(__dirname, process.env.BOOTSTRAP)));
+// app.use(express.static(path.join(__dirname, process.env.FONT_AWESOME)));
 //Log with Morgan in Dev Mode
 if (process.env.NODE_ENV === "developement") {
   app.use(morgan("dev"));
