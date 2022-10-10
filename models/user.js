fc8bcb67e6bd0b86050a664590b7829e1cddc960
Ali Jahankah <aral2@yahoo.com>
2022-10-10 22:24:52 +0100

    using yup for validation

--- models/user.js
+++ models/user.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
+export const userSchema = new mongoose.Schema({
   fullname: {
     type: String,
-    required: true,
+    required: [true, "Please enter your fullname!"],
     trim: true,
     maxLength: 100,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter your email address!"],
     unique: true,
     maxLength: 50,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Please enter your password!"],
     minLength: 8,
     maxLength: 40,
   },
@@ -24,6 +23,7 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -1,42 +1,6 @@
 const { Router } = require("express");
 const router = new Router();
-const Validate = require("fastest-validator");
-const v = new Validate();
-const schema = {
-  fullname: {
-    type: "string",
-    trim: true,
-    min: "4",
-    max: "50",
-    messages: {
-      stringMin: "Full name shoud be more than 4 characters!",
-    },
-  },
-  email: {
-    type: "email",
-    normalize: true,
-    min: "10",
-    messages: {
-      stringMin: "Email shoud be more than 10 characters!",
-    },
-  },
-  password: {
-    type: "string",
-    min: 6,
-    max: 16,
-    messages: {
-      stringMin: "Password must be between 6 to 16 characters!",
-    },
-  },
-  confirmPassword: {
-    type: "string",
-    min: 6,
-    max: 16,
-    messages: {
-      stringMin: "Re-type Password must be between 6 to 16 characters!",
-    },
-  },
-};
+
 router.get("/register", (req, res) => {
   res.render("register", {
     pageTitle: "Register Page",
