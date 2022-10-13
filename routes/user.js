e81945a4a5849d66689eb76246b75ed957b085ec
Ali Jahankah <aral2@yahoo.com>
2022-10-13 22:49:26 +0100

    routes fixed

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = new Router();
 const userController = require("../controllers/userController");
 
-router.get("/register", userController.login);
+router.get("/register", userController.register);
 router.post("/register", userController.createUser);
 router.get("/login", userController.login);
 
